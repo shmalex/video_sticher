9b7bba85db77394fcf210464ca5ecf25738a1655
shmalex <ishmalex@gmail.com>
2022-04-04 14:54:43 -0400

    Update loader

--- concat.py
+++ concat.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import glob
-from PIL import Image, ImageFont, ImageDraw 
+from PIL import Image, ImageFont, ImageDraw
 import os
 import sys
 import tqdm
@@ -12,7 +12,7 @@ import datetime
 
 import os.path
 
-from google.auth.transport.requests import Request 
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -23,35 +23,37 @@ from googleapiclient.http import MediaIoBaseDownload
 import io
 
 
-
 def get_date(path):
     img = Image.open(path)
     if img._getexif() is None:
         return ''
     return img._getexif()[306]
+
+
 title_font = ImageFont.truetype('cour.ttf', 30)
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly','https://www.googleapis.com/auth/drive.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
+          'https://www.googleapis.com/auth/drive.readonly']
 
 if __name__ == "__main__":
     base_dir = sys.argv[1]
-    fps = int(sys.argv[2])
-    dest_dir = os.path.join(base_dir, '..','renamed')
+    folder_id = sys.argv[2]
+    dest_dir = os.path.join(base_dir, '..', 'renamed')
     print('Input:', base_dir)
-    print('FPS:', fps)
     if not os.path.exists(base_dir):
-        print('Invalid path', base_dir)
-        exit(1)
+        print('Path does not exists', base_dir)
+        os.makedirs(base_dir, exist_ok=True)
+        print('Path created', base_dir)
+
     files = glob.glob(base_dir+'*.jpg')
-    print("Total images found",len(files),"sample:")
+    print("Total images found", len(files), "sample:")
     print('\n'.join(files[:5]))
     os.makedirs(dest_dir, exist_ok=True)
 
-    files = list(filter(lambda x: os.path.getsize(x)> 5*10**5, files))
+    files = list(filter(lambda x: os.path.getsize(x) > 5*10**5, files))
     print("Total after filtering", len(files))
     print('Sorting...')
     files = list(sorted(files, key=lambda x: get_date(x)))
-    
-    
+
     ##################### GDrive Credentials
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
@@ -64,9 +66,15 @@ if __name__ == "__main__":
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
+        valid = False
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
+            try:
+                creds.refresh(Request())
+                valid = True
+            except Exception as re:
+                print(re)
+                valid = False
+        if not valid:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials_2.json', SCOPES)
             creds = flow.run_local_server(host="localhost", port=8070)
@@ -74,28 +82,31 @@ if __name__ == "__main__":
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
-
     ##################### GDrive download
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    img = Image.open(files[-1])
-    dt = img._getexif()[306]
-    date, time = dt.split(' ')
-    date = date.replace(':','-')
-    dt = datetime.datetime.fromisoformat(date + ' ' + time)
-    search_dt = (dt + (datetime.datetime.utcnow() - datetime.datetime.now())).isoformat()
-    
-    
+
+    if len(files) > 0:
+        img = Image.open(files[-1])
+        dt = img._getexif()[306]
+        date, time = dt.split(' ')
+        date = date.replace(':', '-')
+        dt = datetime.datetime.fromisoformat(date + ' ' + time)
+        search_dt = (dt + (datetime.datetime.utcnow() -
+                           datetime.datetime.now())).isoformat()
+    else:
+        search_dt = datetime.datetime(1990, 1, 1, 1, 1, 1).isoformat()
+
     service = build('drive', 'v3', credentials=creds)
 
     page_token = None
-
+    print('Search Date', search_dt)
     while True:
         # Call the Drive v3 API
         results = service.files().list(
-            pageSize=100, 
+            pageSize=100,
             fields="nextPageToken, files(*)",
-            q=f"'1vRpM2GnlTqRbpsqnOGJBp18QLKdyk_Gh' in parents  and createdTime > '{search_dt}' " +
-                " and mimeType='image/jpeg'",
+            q=f"'{folder_id}' in parents  and createdTime > '{search_dt}' " +
+            " and mimeType='image/jpeg'",
             orderBy="name",
             pageToken=page_token).execute()
         items = results.get('files', [])
@@ -104,7 +115,8 @@ if __name__ == "__main__":
             print('No files found.')
         with tqdm.tqdm(items) as tq:
             for item in tq:
-                tq.set_description(desc=u'{2} {0} ({1})'.format(item['name'], item['id'], item['createdTime']), refresh=True)
+                tq.set_description(desc=u'{2} {0} ({1})'.format(
+                    item['name'], item['id'], item['createdTime']), refresh=True)
                 file_id = item['id']
                 request = service.files().get_media(fileId=file_id)
                 fh = io.BytesIO()
@@ -112,51 +124,53 @@ if __name__ == "__main__":
                 done = False
                 while done is False:
                     status, done = downloader.next_chunk()
-                f = open(os.path.join(base_dir, item['name']),'wb')
+                f = open(os.path.join(base_dir, item['name']), 'wb')
                 f.write(fh.getbuffer())
                 f.close()
             page_token = results.get('nextPageToken', None)
             if page_token is None:
                 break
 
-
     ##################### Reload Files
 
     files = glob.glob(base_dir+'*.jpg')
-    print("Total images found",len(files),"sample:")
+    print("Total images found", len(files), "sample:")
     print('\n'.join(files[:5]))
     os.makedirs(dest_dir, exist_ok=True)
 
-    files = list(filter(lambda x: os.path.getsize(x)> 5*10**5, files))
+    files = list(filter(lambda x: os.path.getsize(x) > 5*10**5, files))
     print("Total after filtering", len(files))
     print('Sorting...')
     files = list(sorted(files, key=lambda x: get_date(x)))
 
     ##################### Get exif from renamed file
-    
+
     ren_files = glob.glob(dest_dir+'/*.jpg')
     print('Total Renamed Images', len(ren_files))
-    created = {get_date(ren_file):os.path.basename(ren_file) for ren_file in ren_files}
-    
+    created = {get_date(ren_file): os.path.basename(ren_file)
+               for ren_file in ren_files}
+
     ##################### Rename Files, add timestamp
-    
-    n = 7# len(str(len(files)))+1
-    print("name pattern",f'{0:0{n}}.jpg') 
-    
+
+    n = 7  # len(str(len(files)))+1
+    print("name pattern", f'{0:0{n}}.jpg')
+
     for i, file in enumerate(tqdm.tqdm(files, desc='coping')):
-        img=Image.open(file)
+        img = Image.open(file)
         title_text = img._getexif()[306]
         file_name = f'{i:0{n}}.jpg'
         if title_text in created:
-            if created[title_text] == file_name: continue
+            if created[title_text] == file_name:
+                continue
         exif = img.info['exif']
         image_editable = ImageDraw.Draw(img)
-        image_editable.rectangle((10, 15, 360, 46), fill ="#444444")
-        image_editable.text((15,15), title_text, (255, 255, 255), font=title_font)
-        img.save( os.path.join(dest_dir, file_name), exif=exif)
+        image_editable.rectangle((10, 15, 360, 46), fill="#444444")
+        image_editable.text((15, 15), title_text,
+                            (255, 255, 255), font=title_font)
+        img.save(os.path.join(dest_dir, file_name), exif=exif)
 
 #         shutil.copyfile(file, os.path.join(dest_dir, f'{i:0{n}}.jpg'))
-    
+
     if False:
         for i, file in enumerate(tqdm.tqdm(files, desc='coping')):
             shutil.copyfile(file, os.path.join(dest_dir, f'{i:0{n}}.jpg'))
@@ -168,16 +182,4 @@ if __name__ == "__main__":
             out = img.rotate(angle, expand=True)
             out.save(os.path.join('./renamed', f'{i:0n}.jpg'))
     print("Done renaming")
-    print("Running concatination")
-    
-    ##################### Create Video
-
-    target_dir = os.path.abspath(os.path.join(dest_dir, f"%{n}d.jpg"))
-    video_dir = os.path.abspath(os.path.join(dest_dir, '..', f'video_fps{fps}.mp4'))
-    print('Concat Target path', target_dir)
-    bashCommand = f"snap run ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -r {fps} -y -f image2 -i {target_dir}  -tune hq -b:v 20M -bufsize 20M -maxrate 20M -c:v h264_nvenc -crf 30 {video_dir}"
-
-    #     bashCommand = f"ffmpeg -r {fps} -y -f image2 -i {target_dir} -c:v libx264 -crf 30 {video_dir}"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
     print('Exit')
--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
-time ./concat.py ../2_horiz/orig/ 60
+# Aerogarden
+folder_id=1vRpM2GnlTqRbpsqnOGJBp18QLKdyk_Gh
+base_dir=/home/shmalex/Videos/Aerogarden/2_horiz
 
-snap run ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -r 120 -y -f image2 -i /home/shmalex/Videos/Aerogarden/2_horiz/renamed/%7d.jpg  -tune hq -b:v 20M -bufsize 20M -maxrate 20M -c:v h264_nvenc -crf 30 ../2_horiz/video_fps120.mp4
+#backyard 
+folder_id=1XUH7efaFP9cyp_nxW-7Zjn_mnhkmY5v4
+base_dir=/home/shmalex/Videos/Aerogarden/3_backyard
 
-snap run ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -r 240 -y -f image2 -i /home/shmalex/Videos/Aerogarden/2_horiz/renamed/%7d.jpg  -tune hq -b:v 20M -bufsize 20M -maxrate 20M -c:v h264_nvenc -crf 30 ../2_horiz/video_fps240.mp4
+
+
+
+orig_path=$base_dir/orig/
+renamed_dir=$base_dir/renamed
+
+time ./concat.py $orig_path $folder_id
+
+
+snap run ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -r 60 -y -f image2 -i $renamed_dir/%7d.jpg  -tune hq -b:v 20M -bufsize 20M -maxrate 20M -c:v h264_nvenc -crf 30 $base_dir/video_fps120.mp4
+
+snap run ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -r 120 -y -f image2 -i $renamed_dir/%7d.jpg  -tune hq -b:v 20M -bufsize 20M -maxrate 20M -c:v h264_nvenc -crf 30 $base_dir/video_fps120.mp4
+
+snap run ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -r 240 -y -f image2 -i $renamed_dir/%7d.jpg  -tune hq -b:v 20M -bufsize 20M -maxrate 20M -c:v h264_nvenc -crf 30 $base_dir/video_fps240.mp4
